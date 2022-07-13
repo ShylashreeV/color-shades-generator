411fe15a1525af1b56c78b2ccbed474591308ab1
Shylasonu <98378861+Shylasonu@users.noreply.github.com>
2022-07-13 19:08:29 +0530

    commit

--- colorshades/src/App.js
+++ colorshades/src/App.js
@@ -6,19 +6,45 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function App() {
   const [color, setColor] = useState('#ccc');
+  const [shades, setShades] = useState([]);
+  const [showPicker, setShowPicker] = useState(false);
 
   const onChange = (color) => {
     setColor(color.hex);
-
+    const colorValues = new Values(color.hex);
+    const newColors = colorValues.all(10);
+    newColors.pop();
+    setShades(newColors);
   };
+
+  useEffect(() => {
+    onChange({hex: '#ccc'});
+  }, []);
+
   return (
     <div className='wrapper'>
-      <h1 className='heading'> COLOR SHADES GENERATOR ({color}) </h1>
+      <h1 className='heading'> COLOR SHADES GENERATOR <span onClick={() => setShowPicker(!showPicker)}>({color})</span>  </h1>
       <div className='color-picker'>
-        <ChromeColor color={color} onChange={onChange}/>
+        {
+        showPicker && <ChromeColor color={color} onChange={onChange}/>
+        }
       </div>
+        <div className='shades'>
+          <ul className='shades-list'>
+            {
+              shades.map(shade => {
+                return (<CopyToClipboard className="color-code" text={"#" + shade.hex}>
+                    <li style={{ backgroundColor: "#" + shade.hex, color:shade.type === 'shade' ? 'white' : 'black' }}>
+                      {shade.hex}
+                    </li>
+                  </CopyToClipboard>
+                );
+              })
+            }
+          </ul>
+        </div>
     </div>
   )
 }
 
-export default App
+export default App;
